bc187b7b078b7ab3ee75c845f3b8d27e118f6046
Aayush Jain <jaayush014@gmail.com>
2021-06-02 19:56:50 +0530

    initial route and controller

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const authRouter = require('./routes/auth');
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/auth', authRouter);
 
 const PORT = process.env.PORT || 5000;
 
