fbc60166151be573a1a33c13ebd04616dcce4954
Aayush Jain <jaayush014@gmail.com>
2021-06-03 21:38:46 +0530

    custom error handling middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,55 +1,54 @@
 const User = require('../models/User');
+const ErrorResponse = require('../utils/errorResponse');
+const errorResponse = require('../utils/errorResponse');
 
 exports.register = async (req, res, next) => {
-  const { username, email, password } = req.body;
-  try {
-    const user = await User.create({
-      username,
-      email,
-      password,
-    });
-    res.status(201).json({
-      success: true,
-      user,
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      error: err.message,
-    });
-  }
+	const { username, email, password } = req.body;
+	try {
+		const user = await User.create({
+			username,
+			email,
+			password,
+		});
+		res.status(201).json({
+			success: true,
+			user,
+		});
+	} catch (err) {
+		next(error);
+	}
 };
 
 exports.login = async (req, res, next) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res.status(400).json({ sucess: false, error: 'Please provide email and password' });
-  }
-  try {
-    const user = await User.findOne({ email }).select('+password');
-    if (!user) {
-      res.status(404).json({ sucess: false, error: 'Invalid credentials' });
-    }
-    const isMatch = await user.matchPasswords(password);
-    if (!isMatch) {
-      res.status(404).json({ sucess: false, error: 'Invalid credentials' });
-    }
-    res.status(200).json({
-      success: true,
-      token: 'dhbchd',
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      error: err.message,
-    });
-  }
+	const { email, password } = req.body;
+	if (!email || !password) {
+		return next(new ErrorResponse('Please provide an email and password', 400));
+	}
+	try {
+		const user = await User.findOne({ email }).select('+password');
+		if (!user) {
+			return next(new ErrorResponse('Invalid Credentials', 401));
+		}
+		const isMatch = await user.matchPasswords(password);
+		if (!isMatch) {
+			return next(new ErrorResponse('Invalid Credentials', 401));
+		}
+		res.status(200).json({
+			success: true,
+			token: 'dhbchd',
+		});
+	} catch (err) {
+		res.status(500).json({
+			success: false,
+			error: err.message,
+		});
+	}
 };
 
 exports.forgotpassword = (req, res, next) => {
-  res.send('Forgot Password Route');
+	res.send('Forgot Password Route');
 };
 
 exports.resetpassword = (req, res, next) => {
-  res.send('Reset Password Route');
+	res.send('Reset Password Route');
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 const authRouter = require('./routes/auth');
 
 connectDB();
@@ -11,11 +12,13 @@ app.use(express.json());
 
 app.use('/api/auth', authRouter);
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
 
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Logged Error: ${err}`);
-  server.close(() => process.exit(1));
+	console.log(`Logged Error: ${err}`);
+	server.close(() => process.exit(1));
 });
