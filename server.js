c78bfe452af2d9018e6b7fdc65775bda484fc04f
Aayush Jain <jaayush014@gmail.com>
2021-06-03 22:13:29 +0530

    protect middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 const authRouter = require('./routes/auth');
+const privateRouter = require('./routes/private');
 
 connectDB();
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
+app.use('/api/private', privateRouter);
 
 app.use(errorHandler);
 
