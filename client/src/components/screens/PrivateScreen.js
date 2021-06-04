0e5c3ba6bab17f7694f24ce85cde659d1978ebc5
Aayush Jain <jaayush014@gmail.com>
2021-06-04 17:54:40 +0530

    private screen

--- client/src/components/screens/PrivateScreen.js
+++ client/src/components/screens/PrivateScreen.js
@@ -1,7 +1,44 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const PrivateScreen = () => {
-	return <div></div>;
+const PrivateScreen = ({ history }) => {
+	const [error, setError] = useState("");
+	const [privateData, setPrivateData] = useState("");
+
+	useEffect(() => {
+		const fetchPrivateDate = async () => {
+			const config = {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+				},
+			};
+
+			try {
+				const { data } = await axios.get("/api/private", config);
+				setPrivateData(data.data);
+			} catch (error) {
+				localStorage.removeItem("authToken");
+				setError("You are not authorized please login");
+			}
+		};
+
+		fetchPrivateDate();
+	}, []);
+
+	const logoutHandler = () => {
+		localStorage.removeItem("authToken");
+		history.pushState("/login");
+	};
+
+	return error ? (
+		<span className="error-message">{error}</span>
+	) : (
+		<>
+			<div style={{ background: "green", color: "white" }}>{privateData}</div>
+			<button onClick={logoutHandler}>Logout</button>
+		</>
+	);
 };
 
 export default PrivateScreen;
