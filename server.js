3f378dc4eff9c701c774773a540cd1e149381b47
Aayush Jain <jaayush014@gmail.com>
2021-06-05 17:32:09 +0530

    new project setup

---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(
+	fileUpload({
+		useTempFiles: true,
+	})
+);
+
+mongoose.connect(
+	process.env.MONGODB_URI,
+	{
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	(err) => {
+		if (err) throw err;
+		console.log('MongoDB Connected!');
+	}
+);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log('Server is running on port', PORT);
+});
