5e892b59e8cced337a3dae2890adc3a99c44b9df
Aayush Jain <jaayush014@gmail.com>
2021-06-06 18:48:51 +0530

    send email and activation

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,31 +1,60 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const sendMail = require('./sendMail');
 
 const userCtrl = {
 	register: async (req, res, next) => {
 		try {
 			const { name, email, password } = req.body;
 
-			if (!name || !email || !password) return res.status(400).json({ msg: 'Please fill in all fields' });
+			if (!name || !email || !password) return res.status(400).json({ msg: 'Please fill in all fields.' });
 
-			if (!validateEmail(email)) return res.status(400).json({ msg: 'Invalid email' });
+			if (!validateEmail(email)) return res.status(400).json({ msg: 'Invalid emails.' });
 
 			const user = await User.findOne({ email });
-			if (user) res.status(400).json({ msg: 'This email already exists' });
+			if (user) return res.status(400).json({ msg: 'This email already exists.' });
 
-			if (password.length < 6) res.status(400).json({ msg: 'Password must be atleast 6 characters' });
+			if (password.length < 6) return res.status(400).json({ msg: 'Password must be at least 6 characters.' });
 
 			const passwordHash = await bcrypt.hash(password, 12);
 
-			const newUser = { name, email, password: passwordHash };
+			const newUser = {
+				name,
+				email,
+				password: passwordHash,
+			};
 
 			const activation_token = createActivationToken(newUser);
 
 			const url = `${process.env.CLIENT_URL}/user/activate/${activation_token}`;
-			Sendmail(email, url);
+			sendMail(email, url, 'Verify your email address');
 
 			res.json({ msg: 'Register Success! Please activate your email to start.' });
+		} catch (err) {
+			return res.status(500).json({ msg: err.message });
+		}
+	},
+
+	activateEmail: async (req, res) => {
+		try {
+			const { activation_token } = req.body;
+			const user = jwt.verify(activation_token, process.env.ACTIVATION_TOKEN_SECRET);
+
+			const { name, email, password } = user;
+
+			const check = await User.findOne({ email });
+			if (check) return res.status(400).json({ msg: 'This email already exists.' });
+
+			const newUser = new User({
+				name,
+				email,
+				password,
+			});
+
+			await newUser.save();
+
+			res.json({ msg: 'Account has been activated!' });
 		} catch (err) {
 			return res.status(500).json({ msg: err.message });
 		}
