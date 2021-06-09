f429d9a1d8cb4f8a9cdaa4e21530338ebf88e1ea
Aayush Jain <jaayush014@gmail.com>
2021-06-09 17:06:27 +0530

    header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
-	return <div>Header</div>;
+	return (
+		<header>
+			<div className='logo'>
+				<h1>
+					<Link to='/'>Mern Auth</Link>
+				</h1>
+			</div>
+			<ul>
+				<li>
+					<Link to='/'>
+						<i className='fas fa-shopping-cart'></i>Cart
+					</Link>
+				</li>
+				<li>
+					<Link to='/login'>
+						<i className='fas fa-user'></i>Sign in
+					</Link>
+				</li>
+			</ul>
+		</header>
+	);
 }
 
 export default Header;
