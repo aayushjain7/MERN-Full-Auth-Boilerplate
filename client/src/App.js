624a79ab315f8319c8295c3b13a8bc4e345004f1
Aayush Jain <jaayush014@gmail.com>
2021-06-10 17:43:06 +0530

    get access token

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { dispatchLogin, fetchUser, dispatchGetUser } from './redux/actions/authAction';
+import axios from 'axios';
 import Body from './components/body/Body';
 import Header from './components/header/Header';
 
 function App() {
+	const dispatch = useDispatch();
+	const token = useSelector((state) => state.token);
+	const auth = useSelector((state) => state.auth);
+
+	useEffect(() => {
+		const firstLogin = localStorage.getItem('firstLogin');
+		if (firstLogin) {
+			const getToken = async () => {
+				const res = await axios.post('/user/refresh_token', null);
+				dispatch({ type: 'GET_TOKEN', payload: res.data.access_token });
+			};
+			getToken();
+		}
+	}, [auth.isLogged, dispatch]);
+
 	return (
 		<Router>
 			<div className='App'>
